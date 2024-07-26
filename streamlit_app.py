0bdb682f77e7ecd328ef054de59cc53538b52dcc
Brian K Smith <macdaddy_b@me.com>
2024-07-26 18:42:32 +0000

    added radio buttons to change modes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,36 @@ import re
 download_URL = ""
 
 def make_downloadable_GDrive_URL():
-    download_string = "https://drive.google.com/uc?export=download&id="
+    
+    if st.session_state.copy_mode == "Make Copyable":
+        make_copyable_GDrive_URL()
+    else:
+        download_string = "https://drive.google.com/uc?export=download&id="
 
-    file_id_regex = r"\/d\/(.*)\/"
+        file_id_regex = r"\/d\/(.*)\/"
 
-    file_id_match = re.findall(file_id_regex, original_URL_text)
-    print("Text:", original_URL_text)
+        file_id_match = re.findall(file_id_regex, st.session_state['URL'])
 
-    if file_id_match != []:        
-        st.session_state["download_URL"] = download_string + file_id_match[0]
+        if file_id_match != []:        
+            st.session_state["download_URL"] = download_string + file_id_match[0]
+
+def make_copyable_GDrive_URL():
+    new_URL = original_URL_text.replace("edit", "copy")
+    st.session_state["download_URL"] = new_URL
 
 if "download_URL" not in st.session_state:
    st.session_state.download_URL = "Modified URL will appear here"
 
-
-
-
 with st.form("URL Form"):
-    original_URL_text = st.text_input(" ", value="Enter URL to modify", label_visibility="hidden")
+    original_URL_text = st.text_input(" ", value="Enter URL to modify", label_visibility="hidden", key='URL')
+    
+    replacement_mode = st.radio(
+        "Replacement mode",
+        ['Make Downloadable', "Make Copyable"], 
+        index = 0, 
+        horizontal=True,
+        key='copy_mode'
+    )
     downloadable_URL_text = st.code(st.session_state.download_URL)
     submitted = st.form_submit_button("Submit", on_click=make_downloadable_GDrive_URL)
 
